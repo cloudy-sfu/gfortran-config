3944c5646c153371106119e9bc88c25888947eb1
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2024-11-06 23:42:18 +1300

    CMake

--- README.md
+++ README.md
@@ -3,13 +3,19 @@
 Windows batch script to automatically build and run Fortran code in MinGW-w64 `gfortran` environment
 
 ![](https://shields.io/badge/dependencies-MinGW--w64-green)
+![](https://shields.io/badge/dependencies-CMake-green)
 ![](https://shields.io/badge/OS-Windows_10_64--bit-lightgrey)
 
 ## Usage
 
-Open `build_run.bat`, fill in the following fields:
+Open `build_run.bat`, fill in the following variables.
 
-1. The path of the folder which contains `gfortran.exe`, usually `...\mingw64\bin` in installation path of MinGW. 
-2. The root folder of the Fortran program.
+| Name            | Description                                          | Example                                   |
+| --------------- | ---------------------------------------------------- | ----------------------------------------- |
+| `mingw_bin_dir` | The path of the folder which contains `gfortran.exe` | `...\mingw64\bin`                         |
+| `program_root`  | The root folder of the Fortran program.              | `test1`                                   |
+| `cmake_bin_dir` | The path of the folder which contains `cmake.exe`    | `...\cmake-3.31.0-rc3-windows-x86_64\bin` |
+
+*`CMake` is used to automatically analyze dependencies.*
 
 Run `build_run.bat`.
--- build_run.bat
+++ build_run.bat
@@ -2,6 +2,7 @@
 @REM --------------- USER INPUT BEGIN ---------------
 set "mingw_bin_dir="
 set "program_root="
+set "cmake_bin_dir="
 @REM --------------- USER INPUT END   ---------------
 
 if not defined mingw_bin_dir (
@@ -12,11 +13,14 @@ if not defined program_root (
     echo Error: Variable "program_root" is not defined or is empty.
     exit /b 1
 )
+if not defined cmake_bin_dir (
+    echo Error: Variable "cmake_bin_dir" is not defined or is empty.
+    exit /b 1
+)
 set "curdir=%cd%"
 cd %program_root%
 set "PATH=%mingw_bin_dir%;%PATH%"
-set "mingw_gfortran_fn=gfortran.exe"
-set "mingw_gfortran_path=%mingw_bin_dir%\%mingw_gfortran_fn%"
+set "mingw_gfortran_path=%mingw_bin_dir%\gfortran.exe"
 setlocal enabledelayedexpansion
 set "program_name=main"
 set "all_scripts_list="
@@ -43,35 +47,35 @@ for /R %%f in (*.f90) do (
     @REM search program name end
 )
 
-del Makefile 2>nul
+del /Q CMakeLists.txt 2>nul
+set "mingw_gfortran_path_unix=%mingw_gfortran_path:\=/%"
+set "all_scripts_list_unix=%all_scripts_list:\=/%"
 (
     for %%L in (
-        " FC = gfortran"
-        " FFLAGS = -O2"
-        " TARGET = !program_name!.exe"
-        " SRCS = !all_scripts_list!"
-        " OBJS = $(SRCS:.f90=.o)"
-        " DEPS = $(SRCS:.f90=.d)"
-        "."
-        " all: $(TARGET)"
-        "."
-        " $(TARGET): $(OBJS)"
-        " 	$(FC) $(FFLAGS) -o $(TARGET) $(OBJS)"
-        "."
-        " %%.o: %%.f90"
-        " 	$(FC) $(FFLAGS) -c $< -o $@"
-        " 	$(FC) -MM $< > $*.d"
-        "."
-        " -include $(DEPS)"
-        "."
-        " clean:"
-        " 	del /Q $(OBJS) $(DEPS) $(TARGET)"
+        "cmake_minimum_required(VERSION 3.29)"
+        "set(CMAKE_Fortran_COMPILER %mingw_gfortran_path_unix%)"
+        "project(%program_name% Fortran)"
+        "enable_language(Fortran)"
+        "add_executable(%program_name% %all_scripts_list_unix%)"
     ) do (
-        echo%%~L
+        echo %%~L
     )
-) > Makefile
+) > CMakeLists.txt
 
+%cmake_bin_dir%\cmake.exe -G "MinGW Makefiles" %cd%
 %mingw_bin_dir%\mingw32-make.exe
-@REM %program_name%
+
+@REM clean cmake and make files
+del /Q CMakeCache.txt
+del /Q cmake_install.cmake
+del /Q CMakeLists.txt
+del /Q Makefile
+del /Q *.mod
+rmdir /Q /S CMakeFiles
+
+echo.
+%program_name%
+
 endlocal
+
 cd %curdir%
